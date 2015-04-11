1cd9937e2f796ee26b8130c185430dd30c551439
Faizev Hussein <faizev.hussein@gmail.com>
2015-04-11 12:45:45 +1200

    added dependencies and packages

---
+++ server.js
@@ -0,0 +1,9 @@
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var request    = require('request');
