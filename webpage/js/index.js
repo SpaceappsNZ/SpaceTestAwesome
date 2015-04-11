2129c15410c025fa50e004b08593643b15bb5800
Simar Kalra <simarkalra1993@gmail.com>
2015-04-11 21:46:20 +1200

    stuff and stuff

--- webpage/js/index.js
+++ webpage/js/index.js
@@ -14,34 +14,62 @@ function GetImage() {
 	}).then(function (response) {
 		document.getElementById('loader').style.display = 'none'
 		DrawImage(response);
-		AddText();
 	}). catch(function (e, url) {
-		console.log(e);
+		console.log("error");
 	});
 }
 
 function DrawImage(url) {
 	console.log(url);
+	document.getElementById('secondpage').style.display = "";
 	var canvas = document.getElementById('imageCanvas');
-	canvas.style.display = '';
 	canvas.height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 	canvas.width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 	var context = canvas.getContext('2d');
 	var imageObj = new Image();
+	imageObj.addEventListener('load', function () {
+		context.drawImage(imageObj, 0, 0, screen.width, screen.height );
+		AddText();
+	}, false)
 	imageObj.src = url;
-	context.drawImage(imageObj, 0, 0, screen.width, screen.height );
 }
 
 function AddText() {
 	var textToAdd = document.getElementById('event').value;
+	var date = document.getElementById('date').value;
 	var canvas = document.getElementById('imageCanvas');
 	var context = canvas.getContext('2d');
 	context.font = "70px HeadingFontBold";
 
 	var x = screen.width/2 - context.measureText(textToAdd).width/2;
-	var y = screen.height/2 - 75/2 - 50;
+	var y = screen.height/2 - 70/2 - 50;
+	context.fillStyle = "white";
 	context.fillText(textToAdd, x , y);
+	context.font = "50px HeadingFontHairline";
+
+	x = screen.width/2 - context.measureText(date).width/2;
+	y = screen.height/2 - 70/2 + 10;
+
+	context.fillText(date, x, y);
 }
+
+function DownloadFile() {
+
+}
+
+function downloadCanvas(link, canvasId, filename) {
+    link.href = document.getElementById(canvasId).toDataURL();
+    link.download = filename;
+}
+
+/**
+ * The event handler for the link's onclick event. We give THIS as a
+ * parameter (=the link element), ID of the canvas and a filename.
+*/
+document.getElementById('download').addEventListener('click', function() {
+	console.log('click');
+    downloadCanvas(this, 'imageCanvas', 'image.png');
+}, false);
 
 
 
