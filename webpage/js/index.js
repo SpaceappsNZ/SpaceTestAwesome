fccbcd5d1440db71ed5d4341eea0244344d5a0f2
Simar Kalra <simarkalra1993@gmail.com>
2015-04-11 17:29:25 +1200

    did a little bit more

--- webpage/js/index.js
+++ webpage/js/index.js
@@ -4,10 +4,15 @@ function GetImage() {
 	var event = document.getElementById('event').value;
 	var date = document.getElementById('date').value;
 
+	document.getElementById('start').style.display = 'none'
+	var loader = document.getElementById('loader');
+	loader.style.display = '';
+	loader.style.marginTop = (screen.height/2 - 100) + "px";
+
 	qwest.get(urls.getImage, {
 		'date' : date
 	}).then(function (response) {
-		document.getElementById('start').style.display = 'none'
+		document.getElementById('loader').style.display = 'none'
 		var imageDiv = document.getElementById('image');
 		var img = new Image();
 		img.src = response;
@@ -16,3 +21,5 @@ function GetImage() {
 		console.log(e);
 	});
 }
+
+
