196532d66e8f40fa6fc5714eb597ee43cbd7a8a2
Simar Kalra <simarkalra1993@gmail.com>
2015-04-11 22:04:36 +1200

    did stuff

--- server.js
+++ server.js
@@ -7,10 +7,11 @@
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var request    = require('request');
-var datetime  = require('./datetime.js');
+var datetime   = require('./datetime.js');
+var fs         = require('fs');
 
 app.use(express.static(__dirname + '/webpage'));
-console.log(datetime);
+
 app.get('/', function(req,res){
   res.sendFile('index.html');
 });
@@ -18,17 +19,21 @@ app.get('/', function(req,res){
 app.get('/getimage', function(req, res){
   var date = '';
   if (datetime.validate_date(req.query.date)){
-	date = datetime.format_date(req.query.date);
+  	date = datetime.format_date(req.query.date);
   }
-  
+
   var url = "https://api.data.gov/nasa/planetary/apod?api_key=ONlcOyy6fWLG3B84tPKAFYMxYnFMyemDzD5udkTt&date="+date;
-  
+
   request(url, function(err, response, body){
     var dataGram = JSON.parse(body);
-    console.log(dataGram);
-    console.log(err);
 
-    res.send(dataGram.url);
+    var url = dataGram.url;
+    var filename = url.substring(url.lastIndexOf('/')+1);
+
+    request.head(url, function(err, res, body){
+      request(url).pipe(fs.createWriteStream("downloads/" + filename)).on('close', function () {});
+    });
+    res.send("http://localhost:8080/" + "downloads/" + filename);
   });
 });
 
