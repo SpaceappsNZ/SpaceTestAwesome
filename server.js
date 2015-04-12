618634cabb94d2071626959d3f67d98dca4b4739
xmrflipflop <shanghai987@hotmail.com>
2015-04-12 16:11:34 +1200

    added image processing

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var app        = express();                 // define our app using express
 var request    = require('request');
 var datetime   = require('./datetime.js');
 var fs         = require('fs');
+var lwip       = require('lwip');
 
 app.use(express.static(__dirname + '/webpage'));
 
@@ -36,17 +37,64 @@ app.get('/getimage', function(req, res){
 			var filename = url.substring(url.lastIndexOf('/')+1);
 
 			var result = {
-                  'url' : "http://localhost:8080/" + "downloads/" + filename ,
+                  'url' : "http://localhost:8080/" + "images/processed/" + filename ,
                   'info' : dataGram.title
                 }
 
 			request(url).pipe(fs.createWriteStream("webpage/downloads/" + filename)).on('close', function () {
-			 res.send(result);
+        processImg(filename, function(){
+          res.send(result);
+          console.log('Done');
+        });
 			});
 		}
 	}
 	request(url, func);
 });
+
+function resizeBatch (image) {
+  //resize image
+  var maxWidth = 1280;
+  var maxHeight = 800;
+
+  var result = image.batch();
+
+  //rotate portrait
+  if (image.width() < image.height()){
+    result = result.rotate(90);
+  }
+
+  //crop or scale -> blur
+  //resize does not retain aspect
+  return result.cover(maxWidth, maxHeight);
+}
+
+function beautifyBatch (image)
+{
+  //saturation
+  return image.batch().saturate(-1);
+}
+
+function processImg (filename, cb) {
+  lwip.open("webpage/downloads/" + filename, function(err, image){
+    // define a batch of manipulations and save to disk as JPEG:
+    var resizeTask = resizeBatch(image);
+    var beautifyTask = beautifyBatch(image);
+
+    //executes then save to disk
+    resizeTask.exec(function (err, image) {
+      beautifyTask.exec(function (err, image) {
+        image.writeFile("webpage/images/processed/" + filename, function(err){
+          // check err...
+          if (err) console.log(err.message);
+          // done.
+
+          cb();
+        });
+      });
+    });
+  });
+}
 
 app.listen('8080');
 
