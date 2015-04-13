89c5b2c199e717db21d6249a438f50df848dc0d7
Faizev Hussein <faizev.hussein@gmail.com>
2015-04-13 14:38:53 +1200

    random date is assigned if date i is below 16/6/95

--- datetime.js
+++ datetime.js
@@ -8,7 +8,7 @@ module.exports = {
 						} else {
 							return false;
 						}
-					},
+	},
 
 	//takes a date of format dd/mm/yyyy and returns a date of format
 	//yyyy-mm-dd
@@ -16,15 +16,23 @@ module.exports = {
 					var date_list = date.split("/");
 					var new_date = date_list[2] + "-" + date_list[1] + "-" + date_list[0];
 					return new_date;
-				},
-				
+	},
+
 	change_date: function (date) {
 					var date_list = date.split("-");
 					date_list[2] = parseInt(date_list[2]) + 1;
-					if (date_list[2] > 28){
-						date_list[2] = 1;
+
+					if(parseInt(date_list[0])>=1995&&parseInt(date_list[1])>=6&&parseInt(date_list[2])>=16){
+						if (date_list[2] > 28){
+								date_list[2] = 1;
+						}
+					}else{
+						date_list[0] = parseInt(1996 + Math.random() * (2014 - 1995));
+						date_list[1] = parseInt(1 + Math.random() * (12 - 1));
+						date_list[2] = parseInt(1 + Math.random() * (28 - 1));
 					}
-					var return_date = date_list[0]+'-'+date_list[1]+'-'+date_list[2];
+
+					return_date = date_list[0]+'-'+date_list[1]+'-'+date_list[2];
 					return return_date;
 	}
 }
